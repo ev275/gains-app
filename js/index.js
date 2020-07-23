af9b8ad54a0d52f2383cfb3e2c5476d5f850c011
ev275 <enrique.valencia@yale.edu>
2020-07-22 23:16:27 -0500

    worked on ui, new button, form, and cleaned old html

--- js/index.js
+++ js/index.js
@@ -1,11 +1,42 @@
-const hideNewBtn = document.querySelector("#hideNewBtn")
+const newBtn = document.querySelector("#newBtn")
 const newContainer = document.querySelector("#newContainer")
+const newWorkoutForm = document.querySelector("#newWorkoutForm")
+
+function toggleElement(element) {
+  let x = element;
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function eventFire(el, etype){
+  if (el.fireEvent) {
+    el.fireEvent('on' + etype);
+  } else {
+    var evObj = document.createEvent('Events');
+    evObj.initEvent(etype, true, false);
+    el.dispatchEvent(evObj);
+  }
+}
 
 toggleElement(newContainer)
-hideNewBtn.addEventListener("click", () => {
-    toggleElement(hideNewBtn)
+newBtn.addEventListener("click", () => {
+    toggleElement(newBtn)
     toggleElement(newContainer)
 })
+
+newWorkoutForm.addEventListener("submit", () => {
+  event.preventDefault()
+  // some function to create workout and render it
+  
+  newWorkoutForm.reset()
+
+  eventFire(newBtn, "click")
+})
+
+
 
 fetch("http://localhost:3000/api/users/8")
 .then( res => res.json())
@@ -17,14 +48,7 @@ fetch("http://localhost:3000/api/users/8")
 
 // hideSection
 
-function toggleElement(element) {
-    let x = element;
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-}
+
 
 // toggleElement("#today")
 // toggleElement("#today")
