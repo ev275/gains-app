d781868dda334fd93065b3a8b4d715d14f074e74
ev275 <enrique.valencia@yale.edu>
2020-07-27 10:06:25 -0500

    cleaned it up and changed cursor

--- js/index.js
+++ js/index.js
@@ -5,12 +5,16 @@ const newWorkoutForm = document.querySelector("#newWorkoutForm")
 const workoutsContainer = document.querySelector("#workoutsDiv")
 
 const modalBtn = document.querySelector("#modalBtn")
-toggleElement(modalBtn)
 const modalLabel = document.querySelector("#modalLabel")
 const updateWorkoutForm = document.querySelector("#editWorkoutForm")
 const closeModalBtn = document.querySelector("#closeModalBtn")
 const deleteExerciseBtn = document.querySelector("#deleteExerciseBtn")
 
+toggleElement(modalBtn)
+renderExercises()
+toggleElement(newContainer)
+
+//Helper Functions
 function toggleElement(element) {
   let x = element;
   if (x.style.display === "none") {
@@ -36,7 +40,135 @@ function fetchUser(id) {
   .then(console.log)
 }
 
-//renders all exercises
+newBtn.addEventListener("click", () => {
+  toggleElement(newBtn)
+  toggleElement(newContainer)
+})
+
+//Create Exercises
+newWorkoutForm.addEventListener("submit", () => {
+  event.preventDefault()
+  // some function to create workout and render it
+  createWorkout(event.target)
+  
+  newWorkoutForm.reset()
+
+  eventFire(newBtn, "click")
+})
+
+function createWorkout(form) {
+  let workoutName = form[0].value
+
+  let [lift1, sets1, reps1, weights1] = [form[1].value, form[2].value, form[3].value, form[4].value]
+
+  let [lift2, sets2, reps2, weights2] = [form[5].value, form[6].value, form[7].value, form[8].value]
+
+  let [lift3, sets3, reps3, weights3] = [form[9].value, form[10].value, form[11].value, form[12].value]
+
+  let [lift4, sets4, reps4, weights4] = [form[13].value, form[14].value, form[15].value, form[16].value]
+  if (workoutName) {
+    fetch("http://localhost:3000/api/exercises", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        name: workoutName,
+        user_id: 1
+      })
+    })
+    .then(res => res.json())
+    .then(newExercise => {
+      // debugger
+      if (lift1 && sets1 && reps1 && weights1) {
+        fetch("http://localhost:3000/api/lifts", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+          },
+          body: JSON.stringify({
+            name: lift1,
+            exercise_id: newExercise.id,
+            sets: sets1,
+            reps: reps1,
+            weights: weights1
+          })
+        })
+      }
+      return newExercise
+    })
+    .then(newExercise =>{
+      if (lift2 && sets2 && reps2 && weights2) {
+        fetch("http://localhost:3000/api/lifts", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+          },
+          body: JSON.stringify({
+            name: lift2,
+            exercise_id: newExercise.id,
+            sets: sets2,
+            reps: reps2,
+            weights: weights2
+          })
+        })
+      }
+      return newExercise
+    })
+    .then(newExercise => {
+      if (lift3 && sets3 && reps3 && weights3) {
+        fetch("http://localhost:3000/api/lifts", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+          },
+          body: JSON.stringify({
+            name: lift3,
+            exercise_id: newExercise.id,
+            sets: sets3,
+            reps: reps3,
+            weights: weights3
+          })
+        })
+      }
+      return newExercise
+    })
+    .then(newExercise => {
+      if (lift4 && sets4 && reps4 && weights4) {
+        fetch("http://localhost:3000/api/lifts", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+          },
+          body: JSON.stringify({
+            name: lift4,
+            exercise_id: newExercise.id,
+            sets: sets4,
+            reps: reps4,
+            weights: weights4
+          })
+        })
+      }
+      return newExercise
+    })
+    .then( newExercise => {
+      debugger
+      fetch(`http://localhost:3000/api/exercises/${newExercise.id}`)
+      .then(res => res.json())
+      .then(createdExercise => {
+        renderExercise(createdExercise)
+      })
+    })
+  }
+  // debugger
+}
+
+//Read Exercises
 function renderExercises() {
   workoutsContainer.innerHTML=""
   fetch("http://localhost:3000/api/exercises")
@@ -45,30 +177,6 @@ function renderExercises() {
     exercises.forEach(exercise => renderExercise(exercise))
   })
 }
-renderExercises()
-
-//update exercise
-updateWorkoutForm.addEventListener("submit", () => {
-  event.preventDefault()
-  // debugger
-  updateWorkout(event.target)
-  updateWorkoutForm.reset()
-  eventFire(closeModalBtn, "click")
-})
-
-deleteExerciseBtn.addEventListener("click", () => {
-  let id = updateWorkoutForm.exerciseid
-
-  fetch(`http://localhost:3000/api/exercises/${id}`, {
-    method: "DELETE"
-  })
-  .then(res => res.json())
-  .then(json => {
-    renderExercises()
-    eventFire(closeModalBtn, "click")
-  })
-})
-
 
 function renderExercise(createdExercise) {
   // debugger
@@ -89,7 +197,8 @@ function renderExercise(createdExercise) {
 
   let tableDiv = document.createElement("div")
   tableDiv.className = "col-lg-6"
-
+  tableDiv.id = "tableDiv"
+  
   tableDiv.addEventListener("click", () => {
     modalBtn.exerciseid = createdExercise.id
     updateWorkoutForm.exerciseid = createdExercise.id
@@ -160,12 +269,20 @@ function renderLift(lift, bodyTag) {
   bodyTag.append(rowTag)
 }
 
+//Update exercise
+updateWorkoutForm.addEventListener("submit", () => {
+  event.preventDefault()
+  // debugger
+  updateWorkout(event.target)
+  updateWorkoutForm.reset()
+  eventFire(closeModalBtn, "click")
+})
+
 function updateWorkout(form) {
   // debugger
   let id = updateWorkoutForm.exerciseid
   // debugger
   // const updateWorkoutForm = document.querySelector("#editWorkoutForm")
-
   let workoutName = form[0].value
 
   let [lift1, sets1, reps1, weights1] = [form[1].value, form[2].value, form[3].value, form[4].value]
@@ -276,133 +393,22 @@ function updateWorkout(form) {
   }
 }
 
-function createWorkout(form) {
-  let workoutName = form[0].value
+//Destroy Exercise
+deleteExerciseBtn.addEventListener("click", () => {
+  let id = updateWorkoutForm.exerciseid
 
-  let [lift1, sets1, reps1, weights1] = [form[1].value, form[2].value, form[3].value, form[4].value]
-
-  let [lift2, sets2, reps2, weights2] = [form[5].value, form[6].value, form[7].value, form[8].value]
-
-  let [lift3, sets3, reps3, weights3] = [form[9].value, form[10].value, form[11].value, form[12].value]
-
-  let [lift4, sets4, reps4, weights4] = [form[13].value, form[14].value, form[15].value, form[16].value]
-  if (workoutName) {
-    fetch("http://localhost:3000/api/exercises", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json"
-      },
-      body: JSON.stringify({
-        name: workoutName,
-        user_id: 1
-      })
-    })
-    .then(res => res.json())
-    .then(newExercise => {
-      // debugger
-      if (lift1 && sets1 && reps1 && weights1) {
-        fetch("http://localhost:3000/api/lifts", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json"
-          },
-          body: JSON.stringify({
-            name: lift1,
-            exercise_id: newExercise.id,
-            sets: sets1,
-            reps: reps1,
-            weights: weights1
-          })
-        })
-      }
-      return newExercise
-    })
-    .then(newExercise =>{
-      if (lift2 && sets2 && reps2 && weights2) {
-        fetch("http://localhost:3000/api/lifts", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json"
-          },
-          body: JSON.stringify({
-            name: lift2,
-            exercise_id: newExercise.id,
-            sets: sets2,
-            reps: reps2,
-            weights: weights2
-          })
-        })
-      }
-      return newExercise
-    })
-    .then(newExercise => {
-      if (lift3 && sets3 && reps3 && weights3) {
-        fetch("http://localhost:3000/api/lifts", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json"
-          },
-          body: JSON.stringify({
-            name: lift3,
-            exercise_id: newExercise.id,
-            sets: sets3,
-            reps: reps3,
-            weights: weights3
-          })
-        })
-      }
-      return newExercise
-    })
-    .then(newExercise => {
-      if (lift4 && sets4 && reps4 && weights4) {
-        fetch("http://localhost:3000/api/lifts", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json"
-          },
-          body: JSON.stringify({
-            name: lift4,
-            exercise_id: newExercise.id,
-            sets: sets4,
-            reps: reps4,
-            weights: weights4
-          })
-        })
-      }
-      return newExercise
-    })
-    .then( newExercise => {
-      // debugger
-      fetch(`http://localhost:3000/api/exercises/${newExercise.id}`)
-      .then(res => res.json())
-      .then(createdExercise => {
-        renderExercise(createdExercise)
-      })
-    })
-  }
-  // debugger
-}
-
-toggleElement(newContainer)
-newBtn.addEventListener("click", () => {
-    toggleElement(newBtn)
-    toggleElement(newContainer)
+  fetch(`http://localhost:3000/api/exercises/${id}`, {
+    method: "DELETE"
+  })
+  .then(res => res.json())
+  .then(json => {
+    renderExercises()
+    eventFire(closeModalBtn, "click")
+  })
 })
 
-newWorkoutForm.addEventListener("submit", () => {
-  event.preventDefault()
-  // some function to create workout and render it
-  createWorkout(event.target)
-  
-  newWorkoutForm.reset()
 
-  eventFire(newBtn, "click")
-})
+
 
 
 
